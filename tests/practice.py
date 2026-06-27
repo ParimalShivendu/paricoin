af88ddaeb6fd9b179cba4038b942280bab5dc4bd
parimalshivendu <parimal.shivendu@gmail.com>
2026-06-27 21:37:25 +0530

    added more code to practice sheet

--- tests/practice.py
+++ tests/practice.py
@@ -1,5 +1,6 @@
 import hashlib
 import json
+import time
 
 def hash(data:dict) -> str:
     raw_data=json.dumps(data, sort_keys=True).encode()
@@ -19,3 +20,29 @@ tran2={
 
 print(f"the hash of first transaction is: {hash(tran1)}")
 print(f"the hash of second transaciton is: {hash(tran2)}")
+
+class Block:
+    def __init__(self, index, transactions, timestamp, previous_hash):
+        self.index=index
+        self.transactions=transactions
+        self.timestamp=timestamp
+        self.previous_hash=previous_hash
+        self.hash=self.compute_hash()
+    
+    def compute_hash(self):
+        return hash({
+            "index":self.index,
+            "transactions":self.transactions,
+            "timestamp":self.timestamp,
+            "previous_hash":self.previous_hash})
+
+block_1=Block(
+    index=1,
+    transactions=[{"reciever":"parimal1", "sender":"shivendu2", "amount":501},
+                  {"reciever":"parimal2", "sender":"shivendu2", "amount":502},
+                  {"reciever":"parimal3", "sender":"shivendu3", "amount":503},],
+    timestamp=time.time(),
+    previous_hash=0)
+print(f"the timestamp of the first block is {block_1.timestamp}")
+print(f"the reciever of 1st trnasaction of the first block is {block_1.transactions[0]['reciever']}")
+print(f"the amount of 3rd trnasaction of the first block is {block_1.transactions[2]['amount']}")
